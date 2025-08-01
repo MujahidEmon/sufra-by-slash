ed8aeedfff35b7b008db1ceb704de1be187c677a
MujahidEmon <mujahidulislam.official@gmail.com>
2025-08-01 10:49:50 +0600

    basic setup and added navabr and footer

---
+++ src/Pages/Root.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Components/Footer/Footer';
+import Navbar from '../Components/Navbar/Navbar';
+
+const Root = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className='max-w-7xl mx-auto min-h-[calc(100vh-295px)]'>
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
