7f688859e18d4447922bdc829424e6bee6124e08
MujahidEmon <mujahidulislam.official@gmail.com>
2025-08-22 10:31:10 +0600

    modified checkout page

--- src/Pages/CheckoutPage/Checkout.jsx
+++ src/Pages/CheckoutPage/Checkout.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GiConfirmed } from "react-icons/gi";
 import { AuthContext } from "../../Provider/AuthProvider";
 import Swal from "sweetalert2";
@@ -10,7 +10,8 @@ const Checkout = () => {
     const { cartProducts, totalPrice, grandTotal, user } = useContext(AuthContext);
     const navigate = useNavigate();
     const [cart] = useCart();
-    const totalCartPrice = cart.reduce((sum, item) => sum += item.price,0)
+    const [paymentMethod, setPaymentMethod] = useState('card')
+    const totalCartPrice = cart.reduce((sum, item) => sum += item.price, 0)
     console.log(totalCartPrice);
 
     const handlePlaceOrder = async (e) => {
@@ -208,15 +209,7 @@ const Checkout = () => {
                                             />
                                         </div>
 
-                                        <div className="sm:col-span-2">
-                                            <button
-                                                type="submit"
-                                                className="w-full flex items-center justify-center gap-3 rounded-lg bg-[#FCAB35] px-6 py-3 text-white text-lg font-semibold hover:bg-[#fcac35dd] focus:outline-none focus:ring-4  transition"
-                                            >
-                                                <GiConfirmed size={22} />
-                                                Place Order
-                                            </button>
-                                        </div>
+
                                     </div>
                                 </div>
 
@@ -225,17 +218,84 @@ const Checkout = () => {
                                     <h3 className="text-2xl font-semibold mb-4 text-gray-200">
                                         Payment
                                     </h3>
-                                    <div className="flex gap-6">
+                                    <div className="flex justify-between gap-6">
+                                        <div className="flex items-center">
+                                            <input type="radio" className="w-5 h-5 cursor-pointer" id="card" name="card"
+                                                value="card"
+                                                checked={paymentMethod === "card"}
+                                                onChange={(e) => setPaymentMethod(e.target.value)}
+                                            />
+                                            <label htmlFor="card" className="ml-4 flex gap-2 cursor-pointer">
+                                                <img src="https://readymadeui.com/images/visa.webp" className="w-12" alt="card1" />
+                                                <img src="https://readymadeui.com/images/american-express.webp" className="w-12" alt="card2" />
+                                                <img src="https://readymadeui.com/images/master.webp" className="w-12" alt="card3" />
+                                            </label>
+                                        </div>
                                         <label className="flex items-center gap-2 cursor-pointer text-gray-300">
                                             <input
                                                 type="radio"
                                                 name="payment-method"
-                                                value="pay-on-delivery"
+                                                value="cod"
+                                                checked={paymentMethod === "cod"}
+                                                onChange={(e) => setPaymentMethod(e.target.value)}
                                                 className="h-5 w-5 text-indigo-600 border-gray-700 focus:ring-indigo-500"
-                                                defaultChecked
+
                                             />
                                             <span>Payment on delivery</span>
                                         </label>
+                                    </div>
+                                    {
+                                        paymentMethod === 'card' &&
+                                        <div className="grid gap-4 mt-4">
+                                            <div>
+                                                <input
+                                                    type="text"
+                                                    placeholder="Cardholder's Name"
+                                                    className="px-4 py-3.5 border-gray-700 bg-gray-900 text-white w-full text-sm border  rounded-md focus:border-purple-500 focus:bg-black outline-0"
+                                                />
+                                            </div>
+                                            <div className="flex border-gray-700 bg-gray-900 border  rounded-md focus-within:border-purple-500 focus-within:bg-transparent overflow-hidden">
+                                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 ml-3" viewBox="0 0 32 20">
+                                                    <circle cx="10" cy="10" r="10" fill="#f93232" />
+                                                    <path
+                                                        fill="#fed049"
+                                                        d="M22 0c-2.246 0-4.312.75-5.98 2H16v.014c-.396.298-.76.634-1.107.986h2.214c.308.313.592.648.855 1H14.03a9.932 9.932 0 0 0-.667 1h5.264c.188.324.365.654.518 1h-6.291a9.833 9.833 0 0 0-.377 1h7.044c.104.326.186.661.258 1h-7.563c-.067.328-.123.66-.157 1h7.881c.039.328.06.661.06 1h-8c0 .339.027.67.06 1h7.882c-.038.339-.093.672-.162 1h-7.563c.069.341.158.673.261 1h7.044a9.833 9.833 0 0 1-.377 1h-6.291c.151.344.321.678.509 1h5.264a9.783 9.783 0 0 1-.669 1H14.03c.266.352.553.687.862 1h2.215a10.05 10.05 0 0 1-1.107.986A9.937 9.937 0 0 0 22 20c5.523 0 10-4.478 10-10S27.523 0 22 0z"
+                                                    />
+                                                </svg>
+                                                <input
+                                                    type="number"
+                                                    placeholder="Card Number"
+                                                    className="px-4 py-3.5 text-white w-full text-sm outline-0 bg-transparent"
+                                                />
+                                            </div>
+
+                                            <div className="grid grid-cols-2 gap-4">
+                                                <div>
+                                                    <input
+                                                        type="number"
+                                                        placeholder="EXP."
+                                                        className="px-4 py-3.5 border-gray-700 bg-gray-900 text-white w-full text-sm border  rounded-md focus:border-purple-500 focus:bg-transparent outline-0"
+                                                    />
+                                                </div>
+                                                <div>
+                                                    <input
+                                                        type="number"
+                                                        placeholder="CVV"
+                                                        className="px-4 py-3.5 border-gray-700 bg-gray-900 text-white w-full text-sm border  rounded-md focus:border-purple-500 focus:bg-transparent outline-0"
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    }
+
+                                    <div className="sm:col-span-2 mt-4">
+                                        <button
+                                            type="submit"
+                                            className="w-full flex items-center justify-center gap-3 rounded-lg bg-[#FCAB35] px-6 py-3 text-white text-lg font-semibold hover:bg-[#fcac35dd] focus:outline-none focus:ring-4  transition"
+                                        >
+                                            <GiConfirmed size={22} />
+                                            {paymentMethod === 'card' ?' Pay Now ': 'Place Order'}
+                                        </button>
                                     </div>
                                 </div>
                             </div>
